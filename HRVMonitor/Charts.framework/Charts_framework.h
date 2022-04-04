51f71b3e7eab2207b67e857415c32e9803d38980
Blake Chalpin <68305121+b-chalpin@users.noreply.github.com>
2022-04-04 14:49:07 -0700

    Iteration 2 UI with added images - missing Graph and working on removing button background (#32)
    Co-authored-by: MadisonReese <madiraylene@outlook.com>

---
+++ HRVMonitor/Charts.framework/Charts_framework.h
@@ -0,0 +1,18 @@
+//
+//  Charts_framework.h
+//  Charts.framework
+//
+//  Created by Madison R Reese on 3/23/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Charts_framework.
+FOUNDATION_EXPORT double Charts_frameworkVersionNumber;
+
+//! Project version string for Charts_framework.
+FOUNDATION_EXPORT const unsigned char Charts_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Charts_framework/PublicHeader.h>
+
+
--- HRVMonitor/HRVMonitor WatchKit Extension/ContentView.swift
+++ HRVMonitor/HRVMonitor WatchKit Extension/ContentView.swift
@@ -25,23 +25,75 @@ struct ContentView : View {
     }
     
     var body: some View {
-        Form {
-            Section {
-                VStack{
-                    Text("HRV")
-                        .fontWeight(.semibold)
-                        .font(.largeTitle)
-                        .foregroundColor(calculateColor())
-                        .multilineTextAlignment(.center)
-                    Text(getHrvValueString())
-                        .fontWeight(.semibold)
-                        .foregroundColor(calculateColor())
-                        .multilineTextAlignment(.center)
+        Section {
+            TabView{
+                //Page 1
+                ZStack{
+                    calculateHeart()
+                        .resizable()
+                        .frame(width: 25,
+                               height: 22,
+                            alignment: .topTrailing)
+                        .offset(x: 75, y: -70)
+                    VStack(spacing: 10){
+                        Text(getHrvValueString())
+                            .fontWeight(.semibold)
+                            .font(.system(size: 50))
+                            .foregroundColor(calculateColor())
+                            .multilineTextAlignment(.center)
+                            .padding(.top, 30.0)
+                            .padding(.bottom, 0.0)
+                        Text("HRV")
+                            .fontWeight(.semibold)
+                            .foregroundColor(calculateColor())
+                            .multilineTextAlignment(.center)
+                            .padding(.bottom, 10.0)
+                       
+                        
+                        displayStopStartButton()
+                           .padding(.bottom, 5.0)
+                
+                    }
+                    .padding()
                 }
-                .padding()
-            }
-            Section {
-                displayStopStartButton()
+                //Page2
+                VStack {
+                    HStack{
+                        Text(getHrvValueString())
+                            .fontWeight(.semibold)
+                            .font(.system(size: 40))
+                            .foregroundColor(Color("Color"))
+                            .frame(maxHeight: .infinity,
+                                   alignment: .topLeading)
+                        Text("HRV")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 20))
+                            .foregroundColor(Color("Color"))
+                            .frame(maxWidth: .infinity,
+                                   maxHeight: .infinity,
+                                   alignment: .topLeading)
+                            .padding(.top, 18.0)
+                    }
+                    //enter graph here
+                    Text("minimum" + "\nmaximum" + "\naverage")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 16))
+                        .foregroundColor(Color("Color"))
+                        .frame(maxWidth: .infinity,
+                               maxHeight: .infinity,
+                               alignment: .bottomLeading)
+                    
+                }
+                //Page 3
+                HStack{
+                    Text("Settings")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 16))
+                        .foregroundColor(Color("Color"))
+                        .frame(maxWidth: .infinity,
+                               maxHeight: .infinity,
+                               alignment: .topLeading)
+                }
             }
         }
         .alert(isPresented: self.$threatDetector.threatDetected) {
@@ -75,17 +127,36 @@ struct ContentView : View {
     }
     
     func displayStopStartButton() -> some View {
-        switch self.hrPoller.status {
-        case HeartRatePollerStatus.starting, HeartRatePollerStatus.active:
-            return Button(action: { stopMonitor() }) {
-                Text("Stop")
-            }
-        case HeartRatePollerStatus.stopped:
-            return Button(action: { startMonitor() }) {
-                Text("Start")
-            }
+        switch self.monitorEngine.status {
+        case MonitorEngineStatus.starting:
+            return AnyView(
+                Button(action: { stopMonitor() }) {
+                    Circle()
+                        .trim(from: 0, to: 0.6)
+                        .stroke(Color.blue, lineWidth: 4)
+                        .frame(width: 20, height:20)
+                        .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+                        .animation(Animation.default.repeatForever(autoreverses:false))
+                        .onAppear() {
+                            self.isLoading = true
+                        }
+                })
+        case MonitorEngineStatus.active:
+            return AnyView(Button(action: { stopMonitor() }) {
+                Text("STOP")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.blue)
+            })
+        case MonitorEngineStatus.stopped:
+            return AnyView(Button(action: { startMonitor() })
+                {
+                Text("START")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.blue)
+            }.background(Color.black))
         }
     }
+
     
     func stopMonitor() {
         self.monitorEngine.stopMonitoring()
@@ -115,13 +186,40 @@ struct ContentView : View {
             }
         }
         else {
-            return Color.gray
+            return Color("Color")
+        }
+    }
+    
+    func calculateHeart() -> Image {
+        if self.monitorEngine.hrPoller.isActive() {
+            // check for the unexpected case where
+            if let hrv = self.monitorEngine.hrPoller.latestHrv?.value {
+                if hrv <= DANGER_HRV_THRESHOLD {
+                    return Image("redHeart")
+                }
+                else if hrv <= WARNING_HRV_THRESHOLD {
+                    return Image("yellowHeart")
+                }
+                // above warning threshold
+                else {
+                    return Image("greenHeart")
+                }
+            }
+            else {
+                fatalError("ERROR - Heart Rate Poller was active but latestHrv was nil")
+            }
+        }
+        else {
+            return Image("grayHeart")
         }
     }
 }
 
+
+
 struct ContentView_Preview : PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
