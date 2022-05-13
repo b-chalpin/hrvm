bfa9e52be028155d44d8274848ff7ab41328b41f
Nick Adams <45801555+Nick-P-Adams@users.noreply.github.com>
2022-05-13 16:06:34 -0700

    Integrating LR
    Integrating LR with Threat Detector

--- HRVMonitor/HRVMonitor WatchKit Extension/services/ThreatDetector.swift
+++ HRVMonitor/HRVMonitor WatchKit Extension/services/ThreatDetector.swift
@@ -10,24 +10,43 @@ import Foundation
 class ThreatDetector : ObservableObject {
     // singleton
     public static let shared: ThreatDetector = ThreatDetector()
+    private let lrModel: LogisticRegression = LogisticRegression()
     
     @Published var threatDetected: Bool = false
     @Published var threatAcknowledged: Bool = false
     
-    public func checkHrvForThreat(hrv: HrvItem) {
-        if self.predict(hrv: hrv.value) {
+    public func checkHrvForThreat(hrvStore: [HrvItem]) {
+        let predicitonSet = [hrvStores.map{$0.value}]
+        
+        if self.predict(predictionSet: predicitonSet) {
             threatDetected = true
         }
     }
     
-    // this method changed to detect if the threat was actaully acknowledged or not
-    public func acknowledgeThreat(feedback: Bool) {
-        print("FEEDBACK: \(feedback) - Threat acked")
-        self.threatAcknowledged = true
+    // returns true for danger; false otherwise
+    private func predict(predictionSet: [[Double]]) -> Bool {
+        let prediction = lrModel.predict(X: predictionSet)
+        
+        // change the constant 0.75 to an enviroment variable
+        // decided on probability for danger threshold < 0.75 is just a placeholder
+        if(prediction[0] < 0.75){
+            return true
+        }
+        
+        return false
     }
     
-    // returns true for danger; false otherwise
-    public func predict(hrv: Double) -> Bool {
-        return hrv <= Settings.DangerHRVThreshold
+    public func fit() {
+        // stubbed out for now
+    }
+    
+    public func error() {
+        // stubbed out for now
+    }
+    
+    // this method changed to detect if the threat was actaully acknowledged or not
+    public func acknowledgeThreat(feedback: Bool, hrvStore) {
+        print("FEEDBACK: \(feedback) - Threat acked")
+        self.threatAcknowledged = true
     }
 }
