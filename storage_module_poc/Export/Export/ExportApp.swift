7a68e23b23774f1140405ac0fac2816e16708991
AndroidDucky <66288159+AndroidDucky@users.noreply.github.com>
2022-05-23 10:48:53 -0700

    Exporting a file to local file system
    TECH - Implement save storage data to local file system

---
+++ storage_module_poc/Export/Export/ExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExportApp.swift
+//  Export
+//
+//  Created by Jared adams on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
