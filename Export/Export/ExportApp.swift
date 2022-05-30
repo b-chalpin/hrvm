5ffe83d3d276e625b4061c71e07ce49cba23bd49
Blake Chalpin <68305121+b-chalpin@users.noreply.github.com>
2022-05-30 13:50:49 -0700

    Uploading Exporting from watch (#68)
    Add example project to file share between watch and iphone
    
    Co-authored-by: AndroidDucky <66288159+AndroidDucky@users.noreply.github.com>

---
+++ Export/Export/ExportApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExportApp.swift
+//  Export
+//
+//  Created by Jared adams on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExportApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
