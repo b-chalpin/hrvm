ed32656a1b156b7dbb6047436638990da901cc40
Dillon Dalton <103046626+ddalton212@users.noreply.github.com>
2023-04-29 13:40:32 -0700

    Adding to companion

---
+++ hrvm/hrvm_companion_app/Tests/hrvm_companion_appTests/hrvm_companion_appTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import hrvm_companion_app
+
+final class hrvm_companion_appTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
