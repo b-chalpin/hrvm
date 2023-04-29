870057a9e1e226406fbdde1c34eeada41ddaafda
Dillon Dalton <103046626+ddalton212@users.noreply.github.com>
2023-04-29 12:54:59 -0700

    Updating source path

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "hrvm_new",
+    name: "Sources/hrvm_new",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "hrvm_new",
-            targets: ["hrvm_new"]),
+            name: "Sources/hrvm_new",
+            targets: ["Sources/hrvm_new"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "hrvm_new"),
+            name: "Sources/hrvm_new"),
         .testTarget(
             name: "hrvm_newTests",
-            dependencies: ["hrvm_new"]),
+            dependencies: ["Sources/hrvm_new"]),
     ]
 )
