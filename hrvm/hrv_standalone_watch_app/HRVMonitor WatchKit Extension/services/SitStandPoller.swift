b4c8285cb6897d1c256c50feed932be6bd5ed7a3
Dillon Dalton <103046626+ddalton212@users.noreply.github.com>
2023-05-08 14:40:30 -0700

    Adding fixes to sit stand poller

--- hrvm/hrv_standalone_watch_app/HRVMonitor WatchKit Extension/services/SitStandPoller.swift
+++ hrvm/hrv_standalone_watch_app/HRVMonitor WatchKit Extension/services/SitStandPoller.swift
@@ -2,57 +2,58 @@ import Foundation
 import HealthKit
 import CoreMotion
 
-// it is assumed that when status is .active, accelerometer data will be defined
+// It is assumed that when status is .active, accelerometer data will be defined
 enum SitStandPollerStatus {
     case stopped
     case starting
     case active
 }
 
-public class SitStandPoller : ObservableObject {
-    // singleton
+public class SitStandPoller: ObservableObject {
+    // Singleton
     public static let shared: SitStandPoller = SitStandPoller()
 
-    // variable to indicate whether we have notified the poller to stop
+    // Variable to indicate whether we have notified the poller to stop
     private var hasBeenStopped: Bool = true
 
-    @Published var latestAcceleration: CMAcceleration? // our current acceleration
-    @Published var accelerationStore: [CMAcceleration] // store our acceleration values that are being calculated
+    @Published var latestAcceleration: CMAcceleration? // Our current acceleration
+    @Published var accelerationStore: [CMAcceleration] // Store our acceleration values that are being calculated
     @Published var status: SitStandPollerStatus
     @Published var authStatus: Bool = false
-    @Published var sitStandChange: Bool = false // flag for change from sitting to standing
-    
+    @Published var sitStandChange: Bool = false // Flag for change from sitting to standing
+
+    private let healthStore: HKHealthStore // HealthKit store
+
     public init() {
         self.status = .stopped
         self.latestAcceleration = nil
         self.accelerationStore = []
+        self.healthStore = HKHealthStore() // Initialize HealthKit store
         self.checkAuthorization()
     }
-    
+
     public func getAccelerationStore() -> [CMAcceleration] {
         return self.accelerationStore
     }
-    
+
     private func checkAuthorization() {
-        let motionActivityManager = CMMotionActivityManager()
-        motionActivityManager.queryActivityStarting(from: Date(), to: Date(), to: OperationQueue.main) { (activities, error) in
-            if error != nil {
-                print("ERROR - Unable to query motion activity. \(error!.localizedDescription)")
-                self.authStatus = false
-            } else {
-                self.authStatus = true
+        let typesToRead: Set<HKObjectType> = [HKObjectType.workoutType()]
+        self.healthStore.requestAuthorization(toShare: nil, read: typesToRead) { (success, error) in
+            if !success {
+                print("ERROR - Unable to request HealthKit authorization. \(error?.localizedDescription ?? "")")
             }
+            self.authStatus = success
         }
     }
-    
+
     public func isActive() -> Bool {
         return self.status == .active
     }
-    
+
     public func poll() {
         self.hasBeenStopped = false
         self.updateStatus(status: .starting)
-        
+
         let motionActivityManager = CMMotionActivityManager()
         motionActivityManager.startActivityUpdates(to: OperationQueue.main) { (activity) in
             if activity != nil {
@@ -66,7 +67,7 @@ public class SitStandPoller : ObservableObject {
             }
         }
     }
-    
+
     public func stop() {
         self.hasBeenStopped = true
         self.updateStatus(status: .stopped)
@@ -76,7 +77,7 @@ public class SitStandPoller : ObservableObject {
         self.accelerationStore = []
         self.sitStandChange = false
         self.latestAcceleration = nil
-        
+
         let motionManager = CMMotionManager()
         motionManager.accelerometerUpdateInterval = 0.1
         motionManager.startAccelerometerUpdates(to: OperationQueue.main) { (accelerometerData, error) in
@@ -86,11 +87,11 @@ public class SitStandPoller : ObservableObject {
                 if accelerometerData != nil {
                     self.latestAcceleration = accelerometerData!.acceleration
                     self.accelerationStore.append(accelerometerData!.acceleration)
-                    
+
                     if self.accelerationStore.count > 10 {
                         self.accelerationStore.removeFirst()
                     }
-                    
+
                     if self.accelerationStore.count == 10 {
                         let avgAcceleration = self.getAverageAcceleration()
                         if avgAcceleration > 1.0 {
@@ -109,8 +110,9 @@ public class SitStandPoller : ObservableObject {
         }
         return sum / Double(self.accelerationStore.count)
     }
-    
+
     private func updateStatus(status: SitStandPollerStatus) {
         self.status = status
     }
 }
+
