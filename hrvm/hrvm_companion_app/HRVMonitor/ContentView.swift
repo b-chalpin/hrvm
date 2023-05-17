9c9e9b6787bde9476e0f57ea329ebd329477e661
Dillon Dalton <103046626+ddalton212@users.noreply.github.com>
2023-05-17 14:14:56 -0700

    Updating companion app to have one view

--- hrvm/hrvm_companion_app/HRVMonitor/ContentView.swift
+++ hrvm/hrvm_companion_app/HRVMonitor/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var session: PhoneExportSession
     
-    @State private var isReachable = "NO"
     @State private var selectedTab = 0
     
     var body: some View {
@@ -36,41 +35,12 @@ struct ContentView: View {
                         }
                     }
                     .foregroundColor(selectedTab == 0 ? Color.accentColor : Color.gray)
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        selectedTab = 1
-                    }) {
-                        VStack {
-                            Image(systemName: "heart.fill")
-                            Text("Stress Data")
-                        }
-                    }
-                    .foregroundColor(selectedTab == 1 ? Color.accentColor : Color.gray)
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        selectedTab = 2
-                    }) {
-                        VStack {
-                            Image(systemName: "gear")
-                            Text("Settings")
-                        }
-                    }
-                    .foregroundColor(selectedTab == 2 ? Color.accentColor : Color.gray)
-                    
                 }
             }
             .overlay(
                 Group {
                     if selectedTab == 0 {
                         ExportView()
-                    } else if selectedTab == 1 {
-                        StressDataView()
-                    } else {
-                        SettingsView()
                     }
                 }
             )
